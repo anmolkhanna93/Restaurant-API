94b58eb8f6708f410fa3b985bc881a921c55ab01
anmolkhanna93 <anmolkhanna93@gmail.com>
2017-12-27 19:55:19 +0530

    Adding Data
    Removing bugs and adding controllers and models for adding data

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import config from './config';
 import routes from './routes';
 
 let app = express();
-app.server = http:createServer(app);
+app.server = http.createServer(app);
 
+// middleware
+// parse application/json
+app.use(bodyParser.json({
+  limit : config.bodyLimit
+}));
 
 app.use('/v1',routes);
 
 app.server.listen(config.port);
-console.log('Started on port ${app.server.address().port}');
+console.log(`Started on port ${app.server.address().port}`);
 
 export default app;
